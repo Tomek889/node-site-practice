0217b806d1d47893df09c6ed73df27aed3180506
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-06-23 15:48:50 +0200

    Create index, contact-me, about and 404 pages

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const http = require("http");
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
+    const { method, url } = req;
+    const parsedUrl = new URL(url, `http://${req.headers.host}`);
+    const pathname = parsedUrl.pathname;
+
+    if (method === "GET" && pathname === "/") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("home");
+    } else if (method === "GET" && pathname === "/about") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("about");
+    } else if (method === "GET" && pathname === "/contact-me") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("contact-me");
+    } else {
+      res.writeHead(404);
+      res.end("Not Found");
+    }
   })
   .listen(8080);
